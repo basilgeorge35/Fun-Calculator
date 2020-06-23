26e96cbea1d12c57127a68f918b1a555b18b90f3
basilgeorge35 <44412753+basilgeorge35@users.noreply.github.com>
2020-06-24 03:02:45 +0530

    Front Controller set-up

--- src/main/java/com/basil/funCal/config/funCalInitializer.java
+++ src/main/java/com/basil/funCal/config/funCalInitializer.java
@@ -4,13 +4,25 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class funCalInitializer implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		// TODO Auto-generated method stub
-		System.out.println("custom Initializer.>>>>>Basil");
-
+		//System.out.println("custom Initializer.>>>>>Basil");
+		
+		XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+		webApplicationContext.setConfigLocation("classpath:fc-servlet.xml");
+		
+		//create Dispatcher servlet
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+		
+		
+		
+		//register Dispatcher servlet with servletContext
+		servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
 	}
 
 }
