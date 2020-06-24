ee061ad8d37e8dbf4364db4824989e38bbbd6abd
basilgeorge35 <44412753+basilgeorge35@users.noreply.github.com>
2020-06-25 02:04:43 +0530

    Migrating form XML to complete Java

--- src/main/java/com/basil/funCal/config/funCalInitializer.java
+++ src/main/java/com/basil/funCal/config/funCalInitializer.java
@@ -2,9 +2,10 @@ package com.basil.funCal.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class funCalInitializer implements WebApplicationInitializer {
@@ -13,8 +14,11 @@ public class funCalInitializer implements WebApplicationInitializer {
 		// TODO Auto-generated method stub
 		//System.out.println("custom Initializer.>>>>>Basil");
 		
-		XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
-		webApplicationContext.setConfigLocation("classpath:fc-servlet.xml");
+		//XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+		//webApplicationContext.setConfigLocation("classpath:fc-servlet.xml");
+		
+		AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+		webApplicationContext.register(funCalInitializerConfiguration.class);
 		
 		//create Dispatcher servlet
 		DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
@@ -22,7 +26,11 @@ public class funCalInitializer implements WebApplicationInitializer {
 		
 		
 		//register Dispatcher servlet with servletContext
-		servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
+		ServletRegistration.Dynamic myCustomDispatcherServlet = servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
+		
+		myCustomDispatcherServlet.setLoadOnStartup(1);
+		myCustomDispatcherServlet.addMapping("/mywebsite.com/*");
+		
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/view/funCal.jsp
@@ -0,0 +1,7 @@
+<html>
+<head>
+<title>How Fun Are You?</title>
+</head>
+<body>Let's get this party started!!
+</body>
+</html>
